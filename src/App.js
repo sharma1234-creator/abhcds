e55a4eb49d6d4b7f018b5adddfe6da605103650a
sharma1234-creator <72900717+sharma1234-creator@users.noreply.github.com>
2021-04-11 23:12:59 +0530

    sdfgh

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
-function App() {
+
+const App = () =>{
+
+  const [inputList,setInputList] = useState("buy Apple");
+
+  const itemEvent = (event) =>{
+    setInputList(event.target.value);
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <div className="main_div">
+         <div>
+             <br/>
+             <h1>ToDo List</h1>
+             <br/>
+             <input type="text" placeholder="Add a Items" onChange={itemEvent}/>
+             <button> + </button>
+
+             <ol>
+               <li>{inputList}</li>
+             </ol>
+         </div>
+       </div>
+    </>
   );
-}
+
+};
+
 
 export default App;
